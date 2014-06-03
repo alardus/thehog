fce71964c5f96ddf198d5f62e235b0ecdb6dcec2
Alexander Bykov <alardus@alardus.org>
2014-06-04 00:59:14 +0400

    little changes for auto refresh content

--- get_sensors_data.py
+++ get_sensors_data.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python
 
-import urllib2
+import urllib2, time
 import xml.etree.ElementTree as ET
 
 sensor = {}
-urllist = ['http://192.168.0.200/']
+time = time.strftime("%H:%M")
+urllist = ['http://192.168.0.200/', 'http://192.168.0.201/', 'http://192.168.0.202/']
 
-with open('index.html', 'w') as html:
-	html.write('')
+with open('sensor.html', 'w') as html:
+	html.write("""
+		<html>
+		<head>
+		<meta http-equiv="refresh" content="60" />
+		<title></title>
+		</head>
+		<body>
+		<h1>Time %s</h1>\n
+		""" %(time))
 
 def getdata(url):
 	query = urllib2.urlopen(url)
@@ -18,17 +27,18 @@ def getdata(url):
 	for child in root:
 		sensor[child.tag] = child.text
 
-	with open('index.html', 'a') as html:
+	with open('sensor.html', 'a') as html:
 		html.write("""
-			<html>
-			<head><title></title></head>
-			<body>
 			<h2>%s</h2>
 			<p>Temperature: %s</p>
 			<p>Humidity: %s</p>
-			</body>
-			</html>
 			""" %(sensor['place'], sensor['temp'], sensor['hum']))
 
 for url in urllist:
 	getdata(url)
+
+with open('sensor.html', 'a') as html:
+	html.write("""
+		</body>
+		</html>
+		""")
