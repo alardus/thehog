1e9e09ed7ab8c37c48dd87b07fd59d2d2f16990b
Alexander Bykov <alardus@alardus.org>
2015-01-11 02:50:42 +0300

    dimmed led for diff. soil moisture levels

--- moisture_sensor.cpp
+++ moisture_sensor.cpp
@@ -3,25 +3,50 @@ Moisture sensor based on Funduino.
 LED will turn on when soil moisture drop below limit.
 */
 
-int sensor_pin = 0; // analog pin 0
-int moisture_limit = 300; // the value after the LED will turn on
- 
-int led_pin = 13;    // the pin that the LED is attached to
-int led_on = 255;    // how bright the LED is (0 min, 255 max)
-int led_off = 0;     // 
+int sensor_pin = 0; // the pin that sensor is attached to (analog pin 0)
+int led_pin = 10;   // the pin that the LED is attached to
+
+/* Set of sensor values 
+Sensor in dry soil   = min   0, max 300
+Sensor in humid soil = min 300, max 700
+Sensor in water      = min 700, max 950
+*/
+int moisture_limit1 = 400; 
+int moisture_limit2 = 300;
+int moisture_limit3 = 200;
+int moisture_limit4 = 100;
+
+
+// Set of LED values = off 0, max 255
+int led_on1 = 200;    
+int led_on2 = 100;
+int led_on3 = 30;
+int led_on4 = 5;
+int led_off = 0;
 
 void setup() {
-   // Serial.begin(9600);  // start serial port
+    Serial.begin(9600);  // start serial port
 }
  
 void loop() {
     int moisture = analogRead(sensor_pin);
-    // Serial.println(moisture); // write sensor data to serial port
-    if(moisture < moisture_limit) {
-        analogWrite(led_pin, led_on);
-        }
-    else   {
-        analogWrite(led_pin, led_off);
+    Serial.println(moisture); // write sensor data to serial port
+    
+    if(moisture > moisture_limit1) {
+      analogWrite(led_pin, led_off);
+        }  
+    else if ((moisture < moisture_limit1) && (moisture > moisture_limit2)) {
+      analogWrite(led_pin, led_on4);
     }
-    delay(2000);
+    else if ((moisture < moisture_limit2) && (moisture > moisture_limit3)) {
+      analogWrite(led_pin, led_on3);
+    }
+    else if ((moisture < moisture_limit3) && (moisture > moisture_limit4)) {
+      analogWrite(led_pin, led_on2);
+    }
+    
+    else   {
+        analogWrite(led_pin, led_on1);
+    }
+    delay(5000);
 }
